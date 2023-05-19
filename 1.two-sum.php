c7817874cde301ca988a6e2e07e53a86c5a5b34f
RohanKid <127714058+RohanKid@users.noreply.github.com>
2023-05-19 16:55:57 +0900

    演習記録１、１３

---
+++ 1.two-sum.php
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=1 lang=php
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $hashMap = [];
+        $count = count($nums);
+        for($i=0;$i<$count;$i++){
+                $secondIndex = $target-$nums[$i];
+                if(isset($hashMap[$secondIndex])){
+                    return array($i,$hashMap[$secondIndex]);
+                }else{
+                    $hashMap[$nums[$i]]=$i;
+                }
+        }
+        return [];
+    }
+}
+// @lc code=end
---
+++ 13.roman-to-integer.php
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=13 lang=php
+ *
+ * [13] Roman to Integer
+ */
+
+// @lc code=start
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function romanToInt($s) {
+        
+    }
+}
+// @lc code=end
+
