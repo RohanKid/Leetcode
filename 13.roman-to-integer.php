b8d2fa55bdb5700a7449037f8a1b7022d5d778f4
RohanKid <127714058+RohanKid@users.noreply.github.com>
2023-05-19 18:00:25 +0900

    演習記録１３

--- 13.roman-to-integer.php
+++ 13.roman-to-integer.php
@@ -12,8 +12,26 @@ class Solution {
      * @return Integer
      */
     function romanToInt($s) {
-        
+        $RomanNumeral = [
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+        ];
+        $Roman = str_split($s);
+        $integer = 0;
+
+        for ($i = 0; $i < count($Roman); $i++) {
+                if (($RomanNumeral[$Roman[$i + 1]] > $RomanNumeral[$Roman[$i]]) && isset($Roman[$i + 1])) {
+                    $integer -= $RomanNumeral[$Roman[$i]];
+                } else {
+                    $integer += $RomanNumeral[$Roman[$i]];
+                }
+        }
+        return $integer;
     }
 }
 // @lc code=end
-
